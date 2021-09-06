916867cac24e7d42944373c5e8c379e91e6fc9df
Lucas Cota <lucascdornelas@gmail.com>
2021-09-06 09:45:18 -0300

    feat: updated function map

--- functions/map.js
+++ functions/map.js
@@ -15,3 +15,40 @@
     Resultado: ["Lucas", "Cota", "Dornelas"].map(fn) === ["L", "C", "D"]
 
 */
+
+const carrinho = [
+    { nome: "Caneta", qtd: 10, preco: 7.99 },
+    { nome: "Impressora", qtd: 0, preco: 649.50 },
+    { nome: "Caderno", qtd: 4, preco: 27.10 },
+    { nome: "Lapis", qtd: 3, preco: 5.82 },
+    { nome: "Tesoura", qtd: 1, preco: 19.20 }
+]
+
+const getNome = element => element.nome;
+
+const nomes = carrinho.map(getNome);
+console.log("Nomes: ", nomes);
+
+const getTotal = element => element.qtd * element.preco;
+
+const totais = carrinho.map(getTotal);
+console.log("Totais: ", totais);
+
+
+/* Implementação da função map */
+Array.prototype.meuMap = function(fn) {
+    const mapped = []
+
+    for(let i = 0; i < this.length; i++){
+        mapped.push(fn(this[i], i, this));
+    }
+
+    return mapped;
+}
+
+
+const nomesMeuMap = carrinho.meuMap(getNome);
+console.log("Nomes: ", nomesMeuMap);
+
+const totaisMeuMap = carrinho.meuMap(getTotal);
+console.log("Totais: ", totaisMeuMap);
